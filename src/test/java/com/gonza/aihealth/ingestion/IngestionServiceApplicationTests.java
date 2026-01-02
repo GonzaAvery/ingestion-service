ecbb584b5edb07be6945c839021222f2f0aefd16
GonzaAvery <gonzaavery1653@gmail.com>
2026-01-02 18:34:08 -0300

    Initial commit: ingestion service skeleton

---
+++ src/test/java/com/gonza/aihealth/ingestion/IngestionServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.gonza.aihealth.ingestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class IngestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/gonza/aihealth/ingestion/TestIngestionServiceApplication.java
@@ -0,0 +1,11 @@
+package com.gonza.aihealth.ingestion;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestIngestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(IngestionServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
